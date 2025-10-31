1c8a46c6ccc3a31c0a6e28a7d28b11359bed7baf
Carlos Anguiano <carlosanguiano137@gmail.com>
2025-10-30 22:52:01 -0700

    add editor initialization

--- src/init.c
+++ src/init.c
@@ -4,15 +4,27 @@
 //save original terminal settings for exit
 struct termios original_terminal_settings;
 
-//initialize main editor struct
-text_editor editor;
+//initialize editor with empty document
+void initialize_editor(text_editor* editor){
+    editor->total_lines = 1;
+    editor->cursor_x = 0;
+    editor->cursor_y = 0;
+    editor->current_mode = normal_mode;
+    editor->command_length = 0;
+    editor->has_unsaved_changes = 0;
+    strcpy(editor->text_lines[0], "");
+    strcpy(editor->filename, "");
+}
 
 int main(){
     printf("starting sim...\n");
 
+    //initialize main editor struct
+    text_editor editor;
+    initialize_editor(&editor);
+    
     int c = read_user_input();
-    printf("recieved %c\n", c);
 
-    printf("terminating.\n");
+    printf("terminating...\n");
     return 0;
 }
